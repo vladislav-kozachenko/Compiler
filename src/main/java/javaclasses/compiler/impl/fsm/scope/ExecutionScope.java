9421e00e3c8767ce10548cebbc49c3bff670943d
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-11-30 14:06:01 +0200

    Added scope parser factory. Implemented execution scope.

--- src/main/java/javaclasses/compiler/impl/fsm/scope/ExecutionScope.java
+++ src/main/java/javaclasses/compiler/impl/fsm/scope/ExecutionScope.java
@@ -3,25 +3,43 @@ package javaclasses.compiler.impl.fsm.scope;
 import javaclasses.compiler.Command;
 import javaclasses.compiler.CompilationError;
 import javaclasses.compiler.fsm.FiniteStateMachine;
+import javaclasses.compiler.impl.SourceCodeParser;
 import javaclasses.compiler.impl.SourceCodeReader;
+import javaclasses.compiler.impl.fsm.scope.parser.ExecutionScopeParserFactory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import static java.util.EnumSet.of;
+import static javaclasses.compiler.impl.fsm.scope.ExecutionScopeState.*;
+
 public class ExecutionScope extends FiniteStateMachine<List<Command>, SourceCodeReader, ExecutionScopeState, CompilationError> {
+
+    private final ExecutionScopeParserFactory parserFactory = new ExecutionScopeParserFactory();
+
+    private final Map<ExecutionScopeState, Set<ExecutionScopeState>> transitions = new HashMap<ExecutionScopeState, Set<ExecutionScopeState>>() {{
+        put(START, of(STATEMENT));
+        put(STATEMENT, of(STATEMENT_END));
+        put(STATEMENT_END, of(STATEMENT, FINISH));
+
+    }};
+
     @Override
-    protected boolean acceptState(SourceCodeReader sourceCodeReader, List<Command> commands, ExecutionScopeState nextState) throws CompilationError {
-        return false;
+    protected boolean acceptState(SourceCodeReader reader, List<Command> commands, ExecutionScopeState nextState) throws CompilationError {
+        final SourceCodeParser parser = parserFactory.getParser(nextState);
+        return parser.parse(reader, commands);
     }
 
     @Override
     protected boolean isFinishState(ExecutionScopeState currentState) {
-        return false;
+        return currentState == FINISH;
     }
 
     @Override
     protected Set<ExecutionScopeState> getPossibleTransitions(ExecutionScopeState currentState) {
-        return null;
+        return transitions.get(currentState);
     }
 
     @Override
--- src/main/java/javaclasses/compiler/impl/fsm/scope/parser/ExecutionScopeParserFactory.java
+++ src/main/java/javaclasses/compiler/impl/fsm/scope/parser/ExecutionScopeParserFactory.java
@@ -1,4 +1,12 @@
 package javaclasses.compiler.impl.fsm.scope.parser;
 
-public class ExecutionScopeParserFactory {
+import javaclasses.compiler.impl.SourceCodeParser;
+import javaclasses.compiler.impl.fsm.scope.ExecutionScopeState;
+
+public class ExecutionScopeParserFactory  {
+
+    public  SourceCodeParser getParser(ExecutionScopeState state) {
+        return null;
+    }
+
 }
