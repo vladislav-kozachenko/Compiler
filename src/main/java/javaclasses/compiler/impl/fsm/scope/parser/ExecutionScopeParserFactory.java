e46905c2e817985d19b987eef4acd50309abf629
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-11-30 15:24:35 +0200

    Implemented execution scope parser factory.

--- src/main/java/javaclasses/compiler/impl/fsm/scope/parser/ExecutionScopeParserFactory.java
+++ src/main/java/javaclasses/compiler/impl/fsm/scope/parser/ExecutionScopeParserFactory.java
@@ -3,10 +3,25 @@ package javaclasses.compiler.impl.fsm.scope.parser;
 import javaclasses.compiler.impl.SourceCodeParser;
 import javaclasses.compiler.impl.fsm.scope.ExecutionScopeState;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static javaclasses.compiler.impl.fsm.scope.ExecutionScopeState.*;
+
 public class ExecutionScopeParserFactory  {
 
+    private final Map<ExecutionScopeState, SourceCodeParser> parsers = new HashMap<ExecutionScopeState, SourceCodeParser>() {{
+        put(STATEMENT, new StatementParser());
+        put(STATEMENT_END, new StatementEndParser());
+    }};
+
+
     public  SourceCodeParser getParser(ExecutionScopeState state) {
-        return null;
+        if (!parsers.containsKey(state)) {
+            throw new IllegalStateException("Parser not found for state: " + state);
+        }
+
+        return parsers.get(state);
     }
 
 }
