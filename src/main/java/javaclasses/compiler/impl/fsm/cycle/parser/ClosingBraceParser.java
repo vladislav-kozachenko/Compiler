5b41fba7234dd4ff5146f646c087f991ae7d0717
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-01 15:24:22 +0200

    Added closing brace parser and command.

---
+++ src/main/java/javaclasses/compiler/impl/fsm/cycle/parser/ClosingBraceParser.java
@@ -0,0 +1,26 @@
+package javaclasses.compiler.impl.fsm.cycle.parser;
+
+import javaclasses.compiler.Command;
+import javaclasses.compiler.CompilationError;
+import javaclasses.compiler.impl.SourceCodeParser;
+import javaclasses.compiler.impl.SourceCodeReader;
+import javaclasses.compiler.impl.command.ClosingBraceCommand;
+
+import java.util.List;
+
+public class ClosingBraceParser implements SourceCodeParser {
+    @Override
+    public boolean parse(SourceCodeReader reader, List<Command> output) throws CompilationError {
+        String code = reader.getRemainingCode();
+
+        if (code.startsWith("}")) {
+
+            output.add(new ClosingBraceCommand());
+            reader.incrementParsePosition(1);
+
+            return true;
+        }
+
+        return false;
+    }
+}
