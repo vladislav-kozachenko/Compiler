7ea2400c354471cdf2d99ac61a0e4ae22c141c65
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-05 12:08:39 +0200

    Added functional tests for functions and binary operators.

--- src/test/java/javaclasses/compiler/CompilerTest.java
+++ src/test/java/javaclasses/compiler/CompilerTest.java
@@ -50,8 +50,78 @@ public class CompilerTest {
 
     @Test
     public void testPrint() throws Exception {
-        CompilationOutput output = new CompilerImpl().compile("print(5);");
+        CompilationOutput output = compiler.compile("print(5);");
         compiler.execute(output);
         Assert.assertEquals("5.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testPrintMaxFunction() throws Exception {
+        CompilationOutput output = compiler.compile("print(max(5, 1));");
+        compiler.execute(output);
+        Assert.assertEquals("5.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testPrintMinFunction() throws Exception {
+        CompilationOutput output = compiler.compile("print(min(5, 1));");
+        compiler.execute(output);
+        Assert.assertEquals("1.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testPrintSumFunction() throws Exception {
+        CompilationOutput output = compiler.compile("print(sum(5, 1));");
+        compiler.execute(output);
+        Assert.assertEquals("6.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testPrintAverageFunction() throws Exception {
+        CompilationOutput output = compiler.compile("print(avg(5, 1, 3));");
+        compiler.execute(output);
+        Assert.assertEquals("3.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testPrintLog10Function() throws Exception {
+        CompilationOutput output = compiler.compile("print(log10(100));");
+        compiler.execute(output);
+        Assert.assertEquals("2.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testOperatorPlus() throws Exception {
+        CompilationOutput output = compiler.compile("print(2+2);");
+        compiler.execute(output);
+        Assert.assertEquals("4.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testOperatorMinus() throws Exception {
+        CompilationOutput output = compiler.compile("print(22-10);");
+        compiler.execute(output);
+        Assert.assertEquals("12.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testOperatorMultiplication() throws Exception {
+        CompilationOutput output = compiler.compile("print(22.2*10);");
+        compiler.execute(output);
+        Assert.assertEquals("222.0\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testOperatorDivision() throws Exception {
+        CompilationOutput output = compiler.compile("print(22/10);");
+        compiler.execute(output);
+        Assert.assertEquals("2.2\r\n", outContent.toString());
+    }
+
+    @Test
+    public void testOperatorExponentiation() throws Exception {
+        CompilationOutput output = compiler.compile("print(10^2);");
+        compiler.execute(output);
+        Assert.assertEquals("100.0\r\n", outContent.toString());
     }
 }
